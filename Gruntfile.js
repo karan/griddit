168e5c29c2f9fdee943cb6c4c7fd5c9b71ef1456
Karan Goel <karan@goel.im>
2014-05-24 17:08:02 -0700

    add grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+
+  // ===========================================================================
+  // CONFIGURE GRUNT ===========================================================
+  // ===========================================================================
+  grunt.initConfig({
+
+    // get the configuration info from package.json ----------------------------
+    // this way we can use things like name and version (pkg.name)
+    pkg: grunt.file.readJSON('package.json'),
+
+    // all of our configuration will go here
+
+    // configure jshint to validate js files -----------------------------------
+    jshint: {
+      options: {
+        reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
+      },
+
+    // when this task is run, lint the Gruntfile and all js files in src
+      build: ['Grunfile.js', 'js/*.js']
+    },
+
+    // configure uglify to minify js files -------------------------------------
+    uglify: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'dist/js/main.min.js': 'js/main.js',
+          'dist/js/vendor/freewall.min.js': 'js/vendor/freewall.js',
+          'dist/js/vendor/jquery.modal.min.js': 'js/vendor/jquery.modal.min.js'
+        }
+      }
+    },
+
+    // configure cssmin to minify css files ------------------------------------
+    cssmin: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'dist/css/style.min.css': 'css/style.css',
+          'dist/css/vendor/jquery.modal.min.css': 'css/vendor/jquery.modal.css'
+        }
+      }
+    }
+
+  });
+
+  grunt.registerTask('default', ['jshint', 'uglify', 'cssmin']);
+
+  // ===========================================================================
+  // LOAD GRUNT PLUGINS ========================================================
+  // ===========================================================================
+  // we can only load these if they are in our package.json
+  // make sure you have run npm install so our app can find these
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+};
